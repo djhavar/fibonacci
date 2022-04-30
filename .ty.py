1a533e29655c4c235589ae39238fb19216afe790
djhavar <90192071+djhavar@users.noreply.github.com>
2022-04-30 16:53:53 +0530

    Add files via upload

---
+++ .ty.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+root =Tk()
+
+root.title("fibbonaci")
+root.geometry("499x940")
+
+root.mainloop()
+
+ +str label_s= Label(root, text="Fibonacci Seriess: ")
+label_f= Label(root)
+label_sp= Label(root)
+
+def Fibonacci():
+n=10
+
+f=0
+
+s=1
+
+sum=0
+
+c=1
+
+while(c <= n);
+label_series["text"] += str(sum) + ""
+
+c = c + 1
+f = s
+s = sum
+sum = f + s
+
+label_f ['text']="flower is fully bloomed"
+label_sp['text']="spirals in right direction are" +
+str(first_no) + " and spirals in left direction are " "\n. 
+total spiral are " + str(sum)
+root , text+"show Fibonacci Series ", command=Fibonacci
+
+btn = Button (root , text="show Fibonacci Series ", command=Fibonacci)
+btn.pack()
+label_s.pack()
+label_f.pack()
+label_sp.pack()
